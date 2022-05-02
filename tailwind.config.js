8bf4dceb7a985f2b06caa50095655f2895619a42
Boss Udomsuktham <bossiccs@Bosss-MacBook-Pro.local>
2022-05-02 21:27:12 +0700

    finished Hulu build

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   purge:['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '3xl': '2000px',
+      }
+    },
   },
-  plugins: [],
+  plugins: [require('tailwind-scrollbar-hide')],
 }
